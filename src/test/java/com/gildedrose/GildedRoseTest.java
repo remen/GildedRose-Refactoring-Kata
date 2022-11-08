c70444118f828028dec42458cb900eccd53e94d0
Pedro Bertazzo <pedro.bertazzo@meltwater.com>
2022-11-08 16:17:34 +0100

    wip

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,11 +7,35 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class GildedRoseTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[]{new Item("foo", 0, 0)};
+    void appleTest_quality() {
+        Item item = updateSingleItem(new Item("Apple", 5, 5));
+        assertEquals(4, item.quality);
+    }
+
+    @Test
+    void appleTest_sellIn() {
+        Item item = updateSingleItem(new Item("Apple", 5, 5));
+        assertEquals(4, item.sellIn);
+    }
+
+    @Test
+    void conjuredAppleTest_quality() {
+        Item item = new Item("Conjured Apple", 5, 5);
+        updateSingleItem(item);
+        assertEquals(3, item.quality);
+    }
+
+    @Test
+    void conjuredAppleTest_sellIn() {
+        Item item = updateSingleItem(new Item("Conjured Apple", 5, 5));
+        assertEquals(4, item.sellIn);
+    }
+
+    private Item updateSingleItem(Item item) {
+        Item[] items = new Item[]{item};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        return item;
     }
 
 }
